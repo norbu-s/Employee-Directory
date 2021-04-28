110fe61180e82b16361322b752dcc292e51f84f1
Norbu Sherpa <norbuts@hotmail.com>
2021-04-28 19:42:31 +1000

    Creatded table

--- employee-directory/src/components/Table.js
+++ employee-directory/src/components/Table.js
@@ -2,12 +2,12 @@ function Table(props) {
     console.log(props)
     return (
         <table>
-            <tr><td>First Name</td><td>Last Name</td></tr>
+            <tr><td>Photo</td><td>First Name</td><td>Last Name</td><td>Phone</td><td>Email</td><td>Location</td><td>Gender</td></tr>
             {
                 //js to cycle to all user print another row with first name and last name.(m)
                 props.users.map(user => {
                     return (
-                        <tr><td>{user.name.first}</td><td>{user.name.last}</td></tr>
+                        <tr><td>{user.picture.thumbnail}</td><td>{user.name.first}</td><td>{user.name.last}</td><td>{user.phone}</td><td>{user.email}</td><td>{user.location.city}</td><td>{user.gender}</td></tr>
                         )
                     })
             }
